06e9fc62b826db34b577535bce7c2d60b6dc37c3
AM123456789 <anishnumberone@gmail.com>
2023-07-18 13:46:42 -0400

    Update CensusPopulationByState.r

--- CensusPopulationByState.r
+++ CensusPopulationByState.r
@@ -1,13 +1,9 @@
-install.packages("tidycensus")
-install.packages("tidyverse")
-install.packages("ggplot2")
-
 
 library(tidycensus)
 library(tidyverse)
 library(ggplot2)
 
-census_api_key("6f46a7532e0317f2318ad51b2aa17a2643edcc72", install = TRUE, overwrite=TRUE)
+census_api_key("API_KEY", install = TRUE, overwrite=TRUE)
 
 
 #v17 <- load_variables(2021, "acs1", cache = TRUE)
